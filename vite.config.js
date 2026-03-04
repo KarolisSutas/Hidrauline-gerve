656b09d5e36db773ccda3226ae46378bf77fbfc8
KarolisSutas <sutaskarolis@gmail.com>
2026-03-04 11:48:00 +0200

    edit: link names and vite file configuration

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,39 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                produktai: resolve(__dirname, 'produktai.html'),
                 kontaktai: resolve(__dirname, 'kontaktai.html'),
                 apie: resolve(__dirname, 'apie.html'),
+                privatumas: resolve(__dirname, 'privatumas.html'),
+                produktai: resolve(__dirname, 'produktai.html'),
                 lengvoji: resolve(__dirname, 'produktai-lengvoji-serija.html'),
                 vidutine: resolve(__dirname, 'produktai-vidutine-serija.html'),
                 sunkioji: resolve(__dirname, 'produktai-sunkioji-serija.html'),
             },
         },
     },
+    server: {
+        open: true,
+    },
+    plugins: [
+        {
+            name: 'html-rewrite',
+            configureServer(server) {
+                server.middlewares.use((req, res, next) => {
+                    const rewrites = {
+                        '/produktai/lengvoji-serija': '/produktai-lengvoji-serija.html',
+                        '/produktai/vidutine-serija': '/produktai-vidutine-serija.html',
+                        '/produktai/sunkioji-serija': '/produktai-sunkioji-serija.html',
+                        '/produktai': '/produktai.html',
+                        '/kontaktai': '/kontaktai.html',
+                        '/apie': '/apie.html',
+                        '/privatumas': '/privatumas.html',
+                    };
+                    if (rewrites[req.url]) {
+                        req.url = rewrites[req.url];
+                    }
+                    next();
+                });
+            },
+        },
+    ],
 });
